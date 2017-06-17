ccae74212091369fe9eb6a410384260658165e17
姜峰 <jiang.f@Ctrip.com>
2017-06-17 22:09:23 +0800

    save

---
+++ ts/component/icon/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+
+export interface IconProps {
+  onClick: () => {}
+}
+
+const Icon = (props: IconProps)  => {
+
+  const classString = {
+
+  }
+
+  return <i {...props} />
+}
+
+export default Icon
